e850cab4c80434470675854577e59e7ba7ec3fd4
Kelvin Bush <kelybush@gmail.com>
2021-01-24 06:07:06 +0300

    post new article

--- app.js
+++ app.js
@@ -35,6 +35,18 @@ app.get("/articles", (req, res) => {
     }))
 });
 
+app.post("/articles", (req, res) => {
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    newArticle.save(err => {
+        if(!err) res.send("Successfully added");
+        else res.send("Something went wrong");
+    });
+})
+
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
