5fe4d59385f8f14c77c7b9114502f0af3dec6f2b
Kelvin Bush <kelybush@gmail.com>
2021-01-24 05:08:21 +0300

    set up mongo

--- app.js
+++ app.js
@@ -10,10 +10,25 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(express.static("public"));
+app.use('*/css', express.static('public/css'));
 
-//TODO
+mongoose.connect("mongo://localhost:27017/wikiDB", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
 
-app.listen(3000, function() {
+const articleSchema = {
+    title: String,
+    content: String
+}
+
+const Article = mongoose.model("Article", articleSchema);
+const article1 = {
+    title: "REST",
+    content: "REST is short for REpresentational State Transfer. IIt's an architectural style for designing APIs."
+}
+
+
+app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
